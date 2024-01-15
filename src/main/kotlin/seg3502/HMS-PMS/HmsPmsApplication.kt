0b42905870b7ca1444a361385b7188ea759ad88e
akraft01 <71739202+akraft01@users.noreply.github.com>
2024-01-14 23:02:04 -0500

    Add files via upload

---
+++ src/main/kotlin/seg3502/HMS-PMS/HmsPmsApplication.kt
@@ -0,0 +1,11 @@
+package seg3502.HMSPMS
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HmsPmsApplication
+
+fun main(args: Array<String>) {
+	runApplication<HmsPmsApplication>(*args)
+}
